c956effdc7be0be949adaba927960067ada6984f
themanvendra00 <b.manvendrasingh2000@gmail.com>
2023-02-03 22:49:23 +0530

    basic things

---
+++ main.js
@@ -0,0 +1,34 @@
+var tabLinks = document.getElementsByClassName("tab-links");
+var tabContents = document.getElementsByClassName("tab-contents");
+const toggle = document.querySelector(".toggle");
+
+toggle.addEventListener("click", (e) => {
+  const html = document.querySelector("html");
+  if (html.classList.contains("dark")) {
+    html.classList.remove("dark");
+    e.target.innerHTML = "Dark Mode";
+  } else {
+    html.classList.add("dark");
+    e.target.innerHTML = "Light Mode";
+  }
+});
+
+function opentab(tabName) {
+  for (tabLink of tabLinks) {
+    tabLink.classList.remove("active-link");
+  }
+  for (tabContent of tabContents) {
+    tabContent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabName).classList.add("active-tab");
+}
+
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu() {
+  sidemenu.style.right = "0";
+}
+function closemenu() {
+  sidemenu.style.right = "-200px";
+}
