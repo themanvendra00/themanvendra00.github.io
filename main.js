3a21fe28c7ee00e77557bc655e6fb157cd8f8a27
themanvendra00 <b.manvendrasingh2000@gmail.com>
2023-02-09 11:16:37 +0530

    one more pushed

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 var tabLinks = document.getElementsByClassName("tab-links");
 var tabContents = document.getElementsByClassName("tab-contents");
-const toggle = document.querySelector(".toggle");
+// const toggle = document.querySelector(".toggle");
 
-toggle.addEventListener("click", (e) => {
-  const html = document.querySelector("html");
-  if (html.classList.contains("dark")) {
-    html.classList.remove("dark");
-    e.target.innerHTML = "Dark Mode";
-  } else {
-    html.classList.add("dark");
-    e.target.innerHTML = "Light Mode";
-  }
-});
+// toggle.addEventListener("click", (e) => {
+//   const html = document.querySelector("html");
+//   if (html.classList.contains("dark")) {
+//     html.classList.remove("dark");
+//     e.target.innerHTML = "Dark Mode";
+//   } else {
+//     html.classList.add("dark");
+//     e.target.innerHTML = "Light Mode";
+//   }
+// });
 
 let docTitle = document.title;
 window.addEventListener("blur", () => {
