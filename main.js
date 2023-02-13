9826d670233c5ef90dbf59c047188976712e39b3
themanvendra00 <b.manvendrasingh2000@gmail.com>
2023-02-13 13:54:37 +0530

    .

--- main.js
+++ main.js
@@ -32,14 +32,14 @@ function opentab(tabName) {
   document.getElementById(tabName).classList.add("active-tab");
 }
 
-var sidemenu = document.getElementById("sidemenu");
+// var sidemenu = document.getElementById("sidemenu");
 
-function openmenu() {
-  sidemenu.style.right = "0";
-}
-function closemenu() {
-  sidemenu.style.right = "-200px";
-}
+// function openmenu() {
+//   sidemenu.style.right = "0";
+// }
+// function closemenu() {
+//   sidemenu.style.right = "-200px";
+// }
 // const username = "themanvendra00";
 GitHubCalendar(".calendar", "themanvendra00");
 
